5682e3c36445c1cc9cd8d34b0122ebc91f8fdf04
adel <65723807+theadell@users.noreply.github.com>
2023-11-14 21:21:25 +0100

    match ipv4 packages

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,20 @@ fn main() -> io::Result<()> {
             // Not IPV4 packet
             continue;
         }
-        eprintln!(
-            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                eprintln!(
+                    "read {} bytes (flags: {:x}, proto: {:x}): {:?}",
+                    nbytes - 4,
+                    flags,
+                    proto,
+                    p
+                );
+            }
+            Err(e) => {
+                eprint!("Ignoring package {:?}", e);
+            }
+        }
     }
     // some comment added
     // Ok(())
